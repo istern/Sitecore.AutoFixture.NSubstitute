b15b6d030078bda77aa40dc620073317ac80cfa8
ts <ts@pentia.dk>
2017-04-04 09:12:21 +0200

    random item name

--- Src/Sitecore.AutoFixture.NSubstitute/ItemSpecimenBuilder.cs
+++ Src/Sitecore.AutoFixture.NSubstitute/ItemSpecimenBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using NSubstitute;
 using Ploeh.AutoFixture.Kernel;
 using Sitecore.Data;
@@ -8,7 +9,6 @@ namespace Sitecore.AutoFixture.NSubstitute
 {
     public class ItemSpecimentBuilder : ISpecimenBuilder
     {
-        public static string DefaultItemName = "SitecoreItemName";
        
         public object Create(object request, ISpecimenContext context)
         {
@@ -16,14 +16,15 @@ namespace Sitecore.AutoFixture.NSubstitute
             {
                 return new NoSpecimen();
             }
-            
+
+            string generatedItemName = Guid.NewGuid().ToString("N");
             var itemId = ID.NewID;
             var language = Substitute.For<Language>();
-            var definition = new ItemDefinition(itemId, DefaultItemName, ID.NewID, ID.NewID);
+            var definition = new ItemDefinition(itemId, generatedItemName, ID.NewID, ID.NewID);
             var data = new ItemData(definition, language, Sitecore.Data.Version.First, new FieldList());
             var database = Substitute.For<Database>();
             var item = Substitute.For<Item>(itemId, data, database);
-            item.Name = DefaultItemName;
+            item.Name = generatedItemName;
 
             return item;
         }
